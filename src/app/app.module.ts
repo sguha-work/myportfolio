a9fc6b951d2255cdaf9355a15171ebceffd2331d
sguha-work <sguha1988.life@gmail.com>
2018-08-31 16:51:21 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { UserInfoComponent } from './components/user-info/user-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    MenuComponent,
+    UserInfoComponent
   ],
   imports: [
     BrowserModule
